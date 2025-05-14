2657f15194cee16467bffaae8396fb26d3fcb4d8
leehanseock <leehanseock@gmail.com>
2025-05-14 11:32:24 +0900

    BookMartket의 9장 유효성 검사 - Bean Validation

--- src/main/java/kr/ac/kopo/leehanseock/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/leehanseock/bookmarket/domain/Book.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.leehanseock.bookmarket.domain;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,13 @@ import java.math.BigDecimal;
 //@NoArgsConstructor //생성자 추가
 @Data
 public class Book {
+    @Pattern(regexp = "ISBN[0-9]+") //정규표현식
     private String bookId; //도서번호
+    @Size(min = 4, max = 50)
     private String name; //도서명
+    @Min(0)
+    @Digits(integer = 8, fraction = 2)
+    @NotNull
     private BigDecimal unitPrice; //단가
     private String author; //저자명
     private String description; //설명
