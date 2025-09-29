f2f5ecc222411686d40a626e852114dd36aa7427
leehanseock <leehanseock@gmail.com>
2025-09-29 11:49:50 +0900

    주문처리1 - orderCustomerInfo.html

---
+++ src/main/java/kr/ac/kopo/leehanseock/bookmarket/service/OrderService.java
@@ -0,0 +1,8 @@
+package kr.ac.kopo.leehanseock.bookmarket.service;
+
+import kr.ac.kopo.leehanseock.bookmarket.domain.Order;
+
+public interface OrderService {
+    void confirmOrder(String bookId, long quantity);// 도서 재고 수량 확인
+    Long saveOrder(Order order);
+}
---
+++ src/main/java/kr/ac/kopo/leehanseock/bookmarket/service/OrderServiceImpl.java
@@ -0,0 +1,34 @@
+package kr.ac.kopo.leehanseock.bookmarket.service;
+
+import kr.ac.kopo.leehanseock.bookmarket.domain.Book;
+import kr.ac.kopo.leehanseock.bookmarket.domain.Order;
+import kr.ac.kopo.leehanseock.bookmarket.repository.BookRepository;
+import kr.ac.kopo.leehanseock.bookmarket.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public void confirmOrder(String bookId, long quantity) {//도서 재고 수 확인
+        Book bookById = bookRepository.getBookById(bookId);
+        Order order = new Order();
+        if(bookById.getUnitsInStock()<quantity){
+            throw new IllegalArgumentException("도서 수량이 부족합니다. 구입 가능한 도서 수량: "+bookById.getUnitsInStock());
+        } else {
+            bookById.setUnitsInStock(bookById.getUnitsInStock()-quantity);
+        }
+    }
+
+    @Override
+    public Long saveOrder(Order order) {
+        Long orderId = orderRepository.saveOrder(order);
+        return orderId;
+    }
+}
